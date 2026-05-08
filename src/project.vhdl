fa84f9b10a3725758dbae1590de4977fd9898510
jnancoo1 <92116647+jnancoo1@users.noreply.github.com>
2026-05-08 10:51:29 -0400

    Update project.vhdl

--- src/project.vhdl
+++ src/project.vhdl
@@ -2,7 +2,7 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
-entity tt_um_example is
+entity tt_um_dtmf_gen is
     port (
         ui_in   : in  std_logic_vector(7 downto 0);
         uo_out  : out std_logic_vector(7 downto 0);
@@ -15,7 +15,7 @@ entity tt_um_example is
         clk     : in  std_logic;
         rst_n   : in  std_logic
     );
-end tt_um_example;
+end tt_um_dtmf_gen;
 
 architecture Behavioral of tt_um_example is
 
