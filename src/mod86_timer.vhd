b540f81cc475b891f5d35fddda3b99ac7e994bae
jnancoo1 <92116647+jnancoo1@users.noreply.github.com>
2026-05-08 12:56:00 -0400

    Update mod86_timer.vhd

--- src/mod86_timer.vhd
+++ src/mod86_timer.vhd
@@ -1,63 +1,25 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 06.05.2026 18:43:27
--- Design Name: 
--- Module Name: mod86_timer - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
+module mod86_timer(
+input wire clk,
+input wire reset,
+input wire ce,
+output wire [7:0] dataout,
+output wire tc
+);
 
+reg [7:0] cnt;
 
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL;
+always @(posedge clk or posedge reset) begin
+  if (reset) begin
+    cnt <= 8'b01010110;
+  end else if (ce) begin
+    if (cnt == 8'b01010110)
+      cnt <= 8'b00000000;
+    else
+      cnt <= cnt + 1;
+  end
+end
 
----- Uncomment the following library declaration if instantiating
----- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+assign dataout = cnt;
+assign tc = (cnt == 8'b01010110);
 
-entity mod86_timer is
-  Port(clk : in std_logic;
-       reset : in std_logic;
-		 ce : in std_logic;
-		 dataout : out std_logic_vector(7 downto 0);
-		 tc : out std_logic);
-end mod86_timer;
-
-architecture Behavioral of mod86_timer is
-
-signal cnt : std_logic_vector(7 downto 0);
-
-begin
-  process(clk,reset,ce,cnt)
-    begin
-	   if(reset = '1')then
-		  cnt <= "01010110";
-      elsif(clk'event and clk = '1') then
-        if(ce = '1')then
-		    if(cnt = "01010110") then
-		      cnt <= "00000000";
-		    else
-			   cnt <= cnt + 1;
-		    end if;
-		  end if;
-	   end if;
-    end process;
-
-dataout <= cnt;
-
-tc <= '1' when cnt = "01010110" else '0';
-end Behavioral;
-
+endmodule
